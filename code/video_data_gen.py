9695b6387aabae432da8741dc69b3dfdabc7a6f1
Ho Yeon Jeong <2580jhy@naver.com>
2023-11-28 17:08:05 +0900

    경로 변수화 및 이전 파일 삭제기능 추가

--- code/video_data_gen.py
+++ code/video_data_gen.py
@@ -1,7 +1,14 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-num = 10
+import os
+
+
+num = int(input('몇번 영상으로 저장할까요? :'))
+LABEL = 'lunch' 
+PATH = 'G:/내 드라이브/Sign_Remaster/Sign_Language_Remaster/data'
+PATH = 'C:/PlayData/sign_remaster/Sign_Language_Remaster/data'
+print(f'현재 파일은 {LABEL} 폴더로 저장됩니다.')
 cap = cv2.VideoCapture(0)  
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -35,8 +42,11 @@ while cap.isOpened():
     # 녹화 시작ds
     if cv2.waitKey(1) & 0xFF == ord('s'):
         recording = True
-        out = cv2.VideoWriter(f'C:/PlayData/sign_remaster/Sign_Language_Remaster/data/bread_house/bread_house_{num}.avi', fourcc, 20.0, (frame.shape[1], frame.shape[0]))
+        
+        out = cv2.VideoWriter(f'{PATH}/{LABEL}/{LABEL}_{num}.avi', fourcc, 20.0, (frame.shape[1], frame.shape[0]))
         print("녹화 시작")
+    else:
+        print('S ERR something wrong')
     # 녹화 중지 및 파일 저장
     if cv2.waitKey(1) & 0xFF == ord('t'):
         if recording:
@@ -46,7 +56,18 @@ while cap.isOpened():
             num += 1    
             if num == 10:
                 break
-    # 녹화 중일 때 프레임 저장
+    # 직전 녹화 파일 삭제
+    if cv2.waitKey(1) & 0xFF == ord('d'):
+        # 파일 삭제
+        file_path = f'{PATH}/{LABEL}/{LABEL}_{num-1}.avi'
+        num-=1
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print(f'{num}번 파일을 삭제했습니다.')
+        else:
+            print('파일 혹은 경로가 존재하지 않습니다.')
+            
+    # 녹화 중일 때 프레임 저장ㅂㅂㅂㅂㅂ
     if recording:
         out.write(hand_landmarks_image)
 
@@ -56,3 +77,4 @@ while cap.isOpened():
 
 cap.release()
 cv2.destroyAllWindows()
+##q
