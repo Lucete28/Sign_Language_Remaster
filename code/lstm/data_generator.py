59add0d0d14f3ae3b54ac0303ac9a09103b1a381
Ho Yeon Jeong <2580jhy@naver.com>
2024-01-26 15:14:57 +0900

    pull and push

---
+++ code/lstm/data_generator.py
@@ -0,0 +1,154 @@
+#  비디오 학습
+import cv2
+import mediapipe as mp
+import numpy as np
+import time, os
+import random
+from googletrans import Translator
+
+def cut_first(text):
+    t = text.split(',')[0]
+    return t
+
+def trans_to_english(text):
+    text = cut_first(text)
+    translator = Translator()
+    result = translator.translate(text, src='ko', dest='en')
+    return result.text
+##############################
+def make_data(act, v_path):
+
+
+    def rotate_image(image, angle, size = 1):
+        height, width = image.shape[:2]
+        center = (width // 2, height // 2)
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, size)
+        rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
+        return rotated_image
+
+    #변수
+    VIDEO_PATH = v_path
+
+    ACTION = trans_to_english(act) # cv2 출력 문제로 영어로 변경
+    seq_length = 30
+    created_time = int(time.time())
+    print('created at :',created_time)
+    os.makedirs(f'dataset/{ACTION}', exist_ok=True)
+
+    # 동영상 파일 열기
+    cap = cv2.VideoCapture(VIDEO_PATH)
+
+    # 동영상이 제대로 열렸는지 확인
+    if not cap.isOpened():
+        print("Error: Could not open video file.")
+        exit()
+
+    # 동영상의 프레임 수와 크기 가져오기
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    rotate_li = [0, 10, -10, 15, -15]
+    speed_li = [1, 2, 3, 4, 5]
+    size_li = [1, 1.25, 1.5]
+    random.shuffle(rotate_li) 
+    random.shuffle(speed_li) 
+    random.shuffle(size_li) 
+
+    ###############################
+
+    # MediaPipe hands model
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    hands = mp_hands.Hands(
+        max_num_hands=2,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5)
+
+    data = []
+    repeat = 1
+    for repeat_rot in range(len(rotate_li)):
+        rotate = rotate_li[repeat_rot]
+        for repeat_sp in range(len(speed_li)):
+            speed = speed_li[repeat_sp]
+            for repeat_si in range(len(size_li)):
+                size = size_li[repeat_si]
+                print(repeat,'번째 실행입니다.', f'speed : {speed}, rotated : {rotate}, size : {size}')
+                repeat +=1 
+        
+                while True:
+                    ret, img = cap.read()
+                    if not ret: # 영상끝나면 종료
+                        break
+                    img = rotate_image(img, rotate, size)
+                    
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                    result = hands.process(img)
+                    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+                    if result.multi_hand_landmarks is not None:
+                        da = [] 
+                        if len(result.multi_hand_landmarks) == 2 or len(result.multi_hand_landmarks) == 1:
+                            d = []
+                            for res in result.multi_hand_landmarks:  # res 잡힌 만큼 (max 손 개수 이하)
+                                mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+                                joint = np.zeros((21, 3))
+                                for j, lm in enumerate(res.landmark):
+                                    joint[j] = [lm.x, lm.y, lm.z]
+
+                                # Compute angles between joints
+                                v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
+                                v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
+                                v = v2 - v1 # [20, 3]
+                                # Normalize v
+                                v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+                                # Get angle using arcos of dot product
+                                angle = np.arccos(np.einsum('nt,nt->n',
+                                    v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
+                                    v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]\
+
+                                angle = np.degrees(angle) # Convert radian to degree
+                                angle = np.array([angle], dtype=np.float32)
+                                d.append(np.concatenate([joint.flatten(),angle.flatten()]))
+                                if len(result.multi_hand_landmarks)==1:
+                                    d.append(np.zeros_like(d[0]))
+                            da.append([np.concatenate(d)])
+                            data.append(np.concatenate(da))        
+
+
+                    cv2.imshow('img', img)
+                    if cv2.waitKey(int(30 / speed)) & 0xFF == ord('q'): # 속도조절
+                        break
+                    # 동영상 속도에 따라 프레임 위치 설정
+                    frame_index = int(cap.get(cv2.CAP_PROP_POS_FRAMES)) + speed
+                    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+                
+                # 동영상 다시재생
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        
+        
+        
+    # 배열화
+    data = np.array(data)
+    data = data.reshape(data.shape[0],data.shape[-1])
+
+    # 시쿼스 분리
+    full_seq_data = []
+    for seq in range(len(data) - seq_length):
+        full_seq_data.append(data[seq:seq + seq_length])
+
+    full_seq_data = np.array(full_seq_data)
+    # print(ACTION, full_seq_data.shape, data.shape) # 데이터 모양 확인
+
+    # 파일 저장
+    if len(full_seq_data.shape) ==3 :
+        np.save(os.path.join(f'dataset/{ACTION}', f'raw_{created_time}'), data)
+        np.save(os.path.join(f'dataset/{ACTION}', f'seq_{created_time}_{full_seq_data.shape[0]}'), full_seq_data)
+        print(ACTION,'saved', full_seq_data.shape, data.shape)
+    cv2.destroyAllWindows()
+
+    # 비정상 폴더 삭제 (빈폴더)
+    if os.path.exists(f'dataset/{ACTION}') and not os.listdir(f'dataset/{ACTION}'):
+        os.rmdir(f'dataset/{ACTION}')  
+        print(f"{f'dataset/{ACTION}'} 빈폴더 삭제")
---
+++ code/lstm/run_generator.py
@@ -0,0 +1,102 @@
+import requests
+import xmltodict
+import json
+from data_generator import trans_to_english
+from datetime import datetime
+from data_generator import *
+
+#####################################################################################################
+
+def page_todo(file_path, key):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+
+        data = {}
+
+    if key not in data: # key 없을때 
+
+        data[key] = dict()
+
+
+        with open(file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+            print(f"{file_path}에 키 '{key}'가 없어서 추가하였습니다.")
+            return 1
+    else: # 키 있으면 해당키의 최근 페이지 반환
+        if data[key]:
+            return max(data[key]) +1 # 있으면 다음페이지 반환
+        else:
+            return 1
+        
+
+def write_txt_log(T_path, text):
+    current_time = datetime.now()
+    formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    with open(T_path, 'w') as file:
+        file.write(f"{formatted_time} ::: {text}\n")
+
+def write_json_log(J_PATH, data):
+    with open(J_PATH, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+        
+#####################################################################################################
+        
+
+
+
+def get_response(P=1): #    해야하는 페이지 받아서 return item_li 반환
+    print(P)
+
+    for _ in range(P):
+        todo_page = page_todo('api_log.json', 'Daily')
+
+        url = 'http://api.kcisa.kr/openapi/service/rest/meta13/getCTE01701'
+        params = {
+            'serviceKey': 'ecc7282e-731e-4aa0-91b1-017535926c8f',
+            'numOfRows': 10,
+            'pageNo': todo_page,
+        }
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            print('요청 성공')
+            write_txt_log('LOG.TXT', f'Page {todo_page} api 요청 성공')
+
+            content_type = response.headers.get('Content-Type')
+            if content_type and 'charset' in content_type:
+                encoding = content_type.split('charset=')[-1].strip()
+            else:
+                encoding = 'utf-8'
+
+            xml_dict = xmltodict.parse(response.text, encoding=encoding)
+            json_response = json.dumps(xml_dict, ensure_ascii=False, indent=2)
+            json_response = json.loads(json_response)
+            print('응답 내용:', json_response)
+            print(type( json_response['response']['body']['items']['item']), json_response['response']['body']['items']['item'])
+            item_li = json_response['response']['body']['items']['item']
+            data_to_log = {}
+            for item in item_li:
+                print(item['title'])
+                make_data(item['title'],item['subDescription'] )
+                data_to_log[todo_page][trans_to_english(item['title'])] = [item['title'],item['subDescription']]
+    
+        else:
+            print(f'요청이 실패했습니다. 응답 코드: {response.status_code}')
+            write_txt_log('LOG.TXT', f'Page {todo_page} api 요청 실패\n 응답 코드: {response.status_code}')
+            break
+
+
+
+
+
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv)==2:
+        get_response(int(sys.argv[1]))
+    else:
+        get_response()
+
