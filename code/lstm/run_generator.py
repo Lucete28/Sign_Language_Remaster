ca399b5bb3b1c943933390ffef79f123faf98bf9
dla1234wns <dla1234wns@naver.com>
2024-03-03 13:32:19 +0900

    commit test

--- code/lstm/run_generator.py
+++ code/lstm/run_generator.py
@@ -30,7 +30,8 @@ def page_todo(file_path, key):
         else:
             return 1
         
-
+####
+print('test')
 def write_txt_log(T_path, text):
     current_time = datetime.now()
     formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
