bbde3a7d150863ac754c5e33163376e27b034dc5
Ho Yeon Jeong <2580jhy@naver.com>
2024-03-08 12:37:38 +0900

    api work test

--- code/lstm/lstm_test.py
+++ code/lstm/lstm_test.py
@@ -45,6 +45,7 @@ class_select = []
 word_list = []
 is_array_threre = False
 CANT_FIND_HAND_COUNT = 0
+thread = False
 while cap.isOpened():
     ret, img = cap.read()
     # img0 = img.copy()
@@ -108,18 +109,19 @@ while cap.isOpened():
 
     else:
         CANT_FIND_HAND_COUNT+=1
-        if CANT_FIND_HAND_COUNT>10 and not thread.is_alive() and is_array_threre: # 손이 안보인지 10 프레임, 이전 스레드 완료, 서버에 배열 있음 
-            #confirm 요청
-            url = 'http://203.250.133.192:8000/confirm'
-            response = requests.get(url).json() ##TODO 과부화 처리
-            if response['CODE']:
-                is_array_threre = response['is_array_here']
-            #     action = actions[response['most_common_pred']]
-            #     print(action)
-            #     word_list.append(action)
-            # else:
-                # action = 'NO DATA'qq
-            CANT_FIND_HAND_COUNT = 0
+        if CANT_FIND_HAND_COUNT>10 and thread :
+            if not thread.is_alive() and is_array_threre: # 손이 안보인지 10 프레임, 이전 스레드 완료, 서버에 배열 있음 
+                #confirm 요청
+                url = 'http://203.250.133.192:8000/confirm'
+                response = requests.get(url).json() ##TODO 과부화 처리
+                if response['CODE']:
+                    is_array_threre = response['is_array_here']
+                #     action = actions[response['most_common_pred']]
+                #     print(action)
+                #     word_list.append(action)
+                # else:
+                    # action = 'NO DATA'qq
+                CANT_FIND_HAND_COUNT = 0
         cv2.putText(img, f'{action.upper()}',org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 255, 255), thickness=2)
 
     cv2.imshow('img', img)
